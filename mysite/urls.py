2e2c16268496a4111a5adab6a6b8ba3186709dff
alokshandilya <alok.shandilya10@gmail.com>
2024-07-11 08:31:56 +0530

    working version 1
    - next: better frontend

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("authentication.urls")),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
