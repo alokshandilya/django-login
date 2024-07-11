efc84bf503dfc6a7ddb81237ddf97fa1e6aa0859
alokshandilya <alok.shandilya10@gmail.com>
2024-07-11 09:14:25 +0530

    fix dashboard

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import signup, dashboard
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path("", signup, name="signup"),
+    path("dashboard/", dashboard, name="dashboard"),
 ]
