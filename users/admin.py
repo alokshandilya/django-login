2cde5b8d08684cc56b85ba7fc7349eda48e9af85
alokshandilya <alok.shandilya10@gmail.com>
2024-07-11 13:20:22 +0530

    formatter

--- users/admin.py
+++ users/admin.py
@@ -2,25 +2,61 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'is_patient', 'is_doctor')
-    list_filter = ('is_staff', 'is_superuser', 'is_patient', 'is_doctor')
+    list_display = (
+        "username",
+        "email",
+        "is_staff",
+        "is_superuser",
+        "is_patient",
+        "is_doctor",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_patient", "is_doctor")
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'profile_picture')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_patient', 'is_doctor')}),
-        ('Address Info', {'fields': ('address_line1', 'city', 'state', 'pincode')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {"fields": ("first_name", "last_name", "email", "profile_picture")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_patient",
+                    "is_doctor",
+                )
+            },
+        ),
+        ("Address Info", {"fields": ("address_line1", "city", "state", "pincode")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_superuser', 'is_active', 'is_patient', 'is_doctor')}
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_superuser",
+                    "is_active",
+                    "is_patient",
+                    "is_doctor",
+                ),
+            },
         ),
     )
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    ordering = ('username',)
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
 
 # Register the CustomUser model with the CustomUserAdmin
 admin.site.register(CustomUser, CustomUserAdmin)
--- users/views.py
+++ users/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import CustomUserCreationForm
 
+
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('dashboard')
+            return redirect("dashboard")
     else:
         form = CustomUserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, "signup.html", {"form": form})
+
 
 def dashboard(request):
     if request.user.is_patient:
-        return render(request, 'patient_dashboard.html')
+        return render(request, "patient_dashboard.html")
     elif request.user.is_doctor:
-        return render(request, 'doctor_dashboard.html')
+        return render(request, "doctor_dashboard.html")
     else:
-        return redirect('login')
+        return redirect("login")
