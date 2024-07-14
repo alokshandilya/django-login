bab55fea753334d986cf090110e65e58b8f6a4aa
alokshandilya <alok.shandilya10@gmail.com>
2024-07-14 07:09:44 +0530

    select one option (doctor, patient)

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
@@ -19,17 +20,24 @@ class CustomUserCreationForm(UserCreationForm):
             "is_doctor",
         )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name in self.fields:
-            self.fields[field_name].required = True
-
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get("password1")
         confirm_password = cleaned_data.get("password2")
+        is_patient = cleaned_data.get("is_patient")
+        is_doctor = cleaned_data.get("is_doctor")
 
         if password != confirm_password:
             raise forms.ValidationError("Password and confirm password do not match")
+
+        if is_patient and is_doctor:
+            raise forms.ValidationError(
+                "You cannot select both Patient and Doctor roles. Please choose one."
+            )
+
+        if not is_patient and not is_doctor:
+            raise forms.ValidationError(
+                "You must select either Patient or Doctor role."
+            )
 
         return cleaned_data
