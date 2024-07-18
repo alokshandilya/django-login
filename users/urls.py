2582fd90cce791ca9fab6ce50f0ba4311e3308a0
alokshandilya <alok.shandilya10@gmail.com>
2024-07-18 10:19:18 +0530

    add url `view_blog_post/<int:post_id>`

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ from .views import (
     blog_detail,
     edit_post,
     delete_post,
+    view_blog_post,
 )
 
 urlpatterns = [
@@ -17,4 +18,5 @@ urlpatterns = [
     path("blog_detail/<int:post_id>/", blog_detail, name="blog_detail"),
     path("edit_post/<int:post_id>/", edit_post, name="edit_post"),
     path("delete_post/<int:post_id>/", delete_post, name="delete_post"),
+    path("view_blog_post/<int:post_id>/", view_blog_post, name="view_blog_post"),
 ]
