e4a3f858ec8dbabf7556c849511788ebd2a9fcae
Alok Shandilya <alok.shandilya10@gmail.com>
2024-07-22 01:52:51 +0530

    add book_appointment url

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,7 @@ from .views import (
     edit_post,
     delete_post,
     view_blog_post,
+    book_appointment,
 )
 
 urlpatterns = [
@@ -19,4 +20,5 @@ urlpatterns = [
     path("edit_post/<int:post_id>/", edit_post, name="edit_post"),
     path("delete_post/<int:post_id>/", delete_post, name="delete_post"),
     path("view_blog_post/<int:post_id>/", view_blog_post, name="view_blog_post"),
+    path("book_appointment/<int:doctor_id>/", book_appointment, name="book_appointment"),
 ]
