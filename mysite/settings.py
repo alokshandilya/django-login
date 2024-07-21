6ac7fae30551c05097091e7ca89f9731a5a082fd
Alok Shandilya <alok.shandilya10@gmail.com>
2024-07-21 22:45:27 +0530

    add localhost, sqlite3 for local development

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,7 +8,10 @@ SECRET_KEY = "django-insecure-*pd6l2=ei(+)bn$g@4d#*h36a7i)!^o1u^7jh!_)qjqb)bjm!0
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-login-eiu9.onrender.com"]
+ALLOWED_HOSTS = [
+    "django-login-eiu9.onrender.com",
+    "localhost"
+]
 
 INSTALLED_APPS = [
     "users",
@@ -52,11 +55,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
-    "default": dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default="postgresql://postgres:postgres@localhost:5432/mysite",
-        conn_max_age=600,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
