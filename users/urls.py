151ba529eb3f4ddc0f5b8f25c531b5dc88ab50a5
Alok Shandilya <alok.shandilya10@gmail.com>
2024-07-22 10:34:47 +0530

    add url appointment_details
    - import views (full)
    - reduce a few LOC

--- users/urls.py
+++ users/urls.py
@@ -1,24 +1,15 @@
 from django.urls import path
-from .views import (
-    signup,
-    dashboard,
-    create_blog_post,
-    view_blog_posts,
-    blog_detail,
-    edit_post,
-    delete_post,
-    view_blog_post,
-    book_appointment,
-)
+from . import views
 
 urlpatterns = [
-    path("", signup, name="signup"),
-    path("dashboard/", dashboard, name="dashboard"),
-    path("create_blog_post/", create_blog_post, name="create_blog_post"),
-    path("view_blog_posts/", view_blog_posts, name="view_blog_posts"),
-    path("blog_detail/<int:post_id>/", blog_detail, name="blog_detail"),
-    path("edit_post/<int:post_id>/", edit_post, name="edit_post"),
-    path("delete_post/<int:post_id>/", delete_post, name="delete_post"),
-    path("view_blog_post/<int:post_id>/", view_blog_post, name="view_blog_post"),
-    path("book_appointment/<int:doctor_id>/", book_appointment, name="book_appointment"),
+    path("", views.signup, name="signup"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("create_blog_post/", views.create_blog_post, name="create_blog_post"),
+    path("view_blog_posts/", views.view_blog_posts, name="view_blog_posts"),
+    path("blog_detail/<int:post_id>/", views.blog_detail, name="blog_detail"),
+    path("edit_post/<int:post_id>/", views.edit_post, name="edit_post"),
+    path("delete_post/<int:post_id>/", views.delete_post, name="delete_post"),
+    path("view_blog_post/<int:post_id>/", views.view_blog_post, name="view_blog_post"),
+    path("book_appointment/<int:doctor_id>/", views.book_appointment, name="book_appointment"),
+    path("appointment_details/<int:appointment_id>/", views.appointment_details, name="appointment_details"),
 ]
